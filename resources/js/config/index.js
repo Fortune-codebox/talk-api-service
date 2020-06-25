5bed7653d41f2463f406a018eb4eac324f1ed518
Uzoma Fortune <iuzoma@edeco.info>
2020-06-25 13:01:53 +0100

    added updates

--- resources/js/config/index.js
+++ resources/js/config/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://talk-1.herokuapp.com/api";
+// export const API_URL = "http://talk-1.herokuapp.com/api";
 
-// export const API_URL = "http://localhost:8000/api";
+export const API_URL = "http://localhost:8000/api"; 
